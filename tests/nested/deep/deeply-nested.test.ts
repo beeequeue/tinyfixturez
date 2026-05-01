4c4304a9339fb58343823ed32fae37ffe0894d0b
bq <adam@haglund.dev>
2026-05-01 19:26:17 +0200

    update tests

--- tests/nested/deep/deeply-nested.test.ts
+++ tests/nested/deep/deeply-nested.test.ts
@@ -1,16 +1,20 @@
 import { getFixtures } from "tinyfixturez"
 import { expect, it } from "vitest"
 
+import { relativeToCwd } from "../../../src/utils.ts"
+
 it("can find fixtures next to the test file", () => {
 	const fixtures = getFixtures(import.meta.dirname)
-	expect(fixtures.find("deep.txt").trim()).toMatchInlineSnapshot(
-		`"This is a deep.txt fixture, which should only be available to \`deeply-nested.test.ts\`"`,
-	)
+
+	const fixturePath = fixtures.find("deep.txt")
+	expect(relativeToCwd(fixturePath)).toMatchInlineSnapshot(`"tests/nested/deep/fixtures/deep.txt"`)
 })
 
 it("files closer to the test file overrides ones with same name above it", () => {
 	const fixtures = getFixtures(import.meta.dirname)
-	expect(fixtures.find("overridden.txt").trim()).toMatchInlineSnapshot(
-		`"This is the overridden.txt fixture, which should be overriding the root fixture's file of the same name."`,
+
+	const fixturePath = fixtures.find("overridden.txt")
+	expect(relativeToCwd(fixturePath)).toMatchInlineSnapshot(
+		`"tests/nested/deep/fixtures/overridden.txt"`,
 	)
 })
